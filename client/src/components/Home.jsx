bbf3f17ed0f3618f18fc64ff00e126dbe33414e2
dan martin <dmartss13@gmail.com>
2017-08-21 16:39:06 -0400

    Home

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div className="home">
+            <h1> Welcome to our Battle Card app. </h1>
+            <h1> This is the ~ Dream ~ Team ~ </h1>
+        </div>
+    )
+}
+
+export default Home;

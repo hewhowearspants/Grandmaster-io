45ba06b936697a10c6be76f03271e9bb1743e0d1
dan martin <dmartss13@gmail.com>
2017-08-21 16:06:54 -0400

    config

---
+++ db/config.js
@@ -0,0 +1,23 @@
+const options={
+    query:(e)=>{
+        console.log(e.query);
+    }
+};
+
+const pgp=require('pg-promise')(options);
+
+function setDatabase(){
+    if(process.env.NODE_ENV==='development'||!process.env.NODE_ENV){
+        return pgp({
+            database:'dream_team_dev',
+            port:5432,
+            host:'localhost',
+        })
+    }else{
+        return pgp(process.env.DATABASE_URL);
+    }
+}
+
+const db=setDatabase();
+
+module.exports=db;

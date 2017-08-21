e02cfd3adcb80704ab66a691736d30cb8237bf76
Carly Warner <cwarner523>
2017-08-21 16:16:11 -0400

    add register component

---
+++ client/src/components/Register.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: '',
+            email: '',
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(e) {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({
+            [name]:value,
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={(e) => this.props.handleRegisterSubmit(e, this.state.username, this.state.password, this.state.email)}>
+                    <input type="text" name="username" value={this.state.username} placeholder="Username" onChange={this.handleInputChange} />
+                    <input type="passowrd" name="password" value={this.state.password} placeholder="Password" onChange={this.handleInputChange} />
+                    <input type="email" name="email" value={this.state.email} placeholder="email" onChange={this.handleInputChange} />
+                    <input type="submit">Register</input>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Register;

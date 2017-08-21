edafcbf8e7e5f9f6901ed20517616559021d4854
dan martin <dmartss13@gmail.com>
2017-08-21 16:13:01 -0400

    initial imports to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
 import './App.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+
 
 class App extends Component {
   render() {

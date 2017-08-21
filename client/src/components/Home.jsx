4bf7c9c96a9c0d25272cdea060df065cfc6a540b
dan martin <dmartss13@gmail.com>
2017-08-21 17:37:36 -0400

    fix home and header

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+import Login from './Login';
+
+
 const Home = () => {
     return(
         <div className="home">
             <h1> Welcome to our Battle Card app. </h1>
-            <h1> This is the ~ Dream ~ Team ~ </h1>
+            <ul>
+                <li><Login /></li>
+                <li>Not logged in? <Link to ='/register'>Register</Link></li>
+            </ul>
         </div>
     )
 }

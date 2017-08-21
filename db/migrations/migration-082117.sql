f43d9cba8b521e0dbe3a72a6b2a334fcf0e787d5
dan martin <dmartss13@gmail.com>
2017-08-21 15:57:44 -0400

    migration tables

---
+++ db/migrations/migration-082117.sql
@@ -0,0 +1,26 @@
+\c dream_team_dev;
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  password_digest TEXT NOT NULL,
+  display_name VARCHAR(255),
+  email VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS users_cards (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  class VARCHAR(255),
+  att INTEGER,
+  defense INTEGER,
+  user_id INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS cards (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  class VARCHAR(255),
+  att INTEGER,
+  defense INTEGER
+);

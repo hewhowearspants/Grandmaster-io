2f30bd1f5fc5b8ca0b300e39c66cac43f0c938a9
Mark Ma <mazheng1022@hotmail.com>
2017-08-21 17:36:12 -0400

    correct insert $4

--- models/user.js
+++ models/user.js
@@ -12,9 +12,9 @@ User.create=user=>{
     return db.one(`
     INSERT INTO users
     (username,password_digest,display_name,email)
-    VALUES ($1,$2,$3)
+    VALUES ($1,$2,$3,$4)
     RETURNING *
-    `,[user.username,user.password_digest,user.display_name,user.email]);
+    `,[user.username,user.password_digest,user.displayName,user.email]);
 };
 
 module.exports=User;

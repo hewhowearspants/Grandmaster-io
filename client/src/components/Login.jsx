79b338c9963a040a802d3955899abb4f9dc60d53
Carly Warner <cwarner523>
2017-08-21 16:05:44 -0400

    add login component

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    constructor () {
+        super();
+        this.state = {
+            username: '',
+            password: '',
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(e) {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={(e) => this.props.handleLoginSubmit(e, this.state.username, this.state.password)}>
+                    <input type="text" name="username" value={this.state.username} placeholder="Username" onChange={this.handleInputChange} />
+                    <input type="password" name="passowrd" valye={this.state.password} placeholder="Password" onChange={this.handleInputChange} />
+                    <input type="submit" value='Log in!' />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Login;

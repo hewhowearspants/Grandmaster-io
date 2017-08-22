77909b127db72ae3777759daa12cc638586daa66
Mark Ma <mazheng1022@hotmail.com>
2017-08-22 10:37:23 -0400

    start the usercards.jsx structure...

---
+++ client/src/components/UserCards.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserCards extends Component {  
+  render() {
+    return (
+      <div className='UserCards'>
+
+      </div>
+    )
+  }
+}
+
+export default UserCards;

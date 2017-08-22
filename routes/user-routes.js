0ea1b639cba56caab2a1d6b0efe78c289e244ead
Mark Ma <mazheng1022@hotmail.com>
2017-08-22 11:45:15 -0400

    removed addToUser route from user-routes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -6,6 +6,6 @@ const authHelpers=require('../services/auth/auth-helpers');
 
 userRoutes.get('/',authHelpers.loginRequired,usersController.index);
 userRoutes.get('/new',authHelpers.loginRequired,cardsController.findTen);
-userRoutes.post('/new',authHelpers.loginRequired,cardsController.addToUser);
+
 
 module.exports=userRoutes;

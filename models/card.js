70550311fbb44f357f99e21e06b319e92356b7ed
dan martin <dmartss13@gmail.com>
2017-08-22 11:53:06 -0400

    card delete

--- models/card.js
+++ models/card.js
@@ -15,4 +15,30 @@ Card.findTen = () => {
   `);
 }
 
+Card.addToUser = (card,userid) => {
+  return db.one(`
+  INSERT INTO users_cards
+  (card_id, name, class, attack, defense, user_id)
+  VALUES ($1, $2, $3, $4, $5, $6)
+  RETURNING *
+  `, [card.cardId, card.name, card.class, card.attack, card.defense, userid]);
+}
+
+Card.update = (card,id)=>{
+    return db.one(`
+    UPDATE users_cards SET
+    name=$1,
+    WHERE card_id=$2
+    RETURNING *
+    `,[card.name, card.cardId])
+}
+
+Card.destroy = (id) => {
+  return db.none(`
+    DELETE FROM users_cards
+    WHERE id=$1
+    `,[id]);
+}
+
+
 module.exports = Card;

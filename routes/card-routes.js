2db51ca0d8cacb5edb4206bfc74c108b2b47d50c
dan martin <dmartss13@gmail.com>
2017-08-22 14:47:43 -0400

    get single card route

--- routes/card-routes.js
+++ routes/card-routes.js
@@ -5,5 +5,6 @@ const authHelpers = require('../services/auth/auth-helpers');
 const cardsController = require('../controllers/cards-controller');
 
 cardRoutes.get('/', cardsController.index);
+cardRoutes.get('/new',authHelpers.loginRequired,cardsController.findOne);
 
 module.exports = cardRoutes;

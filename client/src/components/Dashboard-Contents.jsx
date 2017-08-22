7fd82685d2c711b59e314f9b191e25bcc2a91e0e
Mark Ma <mazheng1022@hotmail.com>
2017-08-22 12:43:32 -0400

    comment out findAllCards and add UserCards component

--- client/src/components/Dashboard-Contents.jsx
+++ client/src/components/Dashboard-Contents.jsx
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+} from 'react-router-dom'
 
 import UserCards from './UserCards';
 
 class DashboardContents extends Component {  
-  renderCardCollection() {
-    if (this.props.cards) {
-      return (this.props.cards.map((card) => {
-          return (
-            <div className='card'>
-              <p>{card.name}</p>
-              <p>{card.class}</p>
-            </div>
-          )
-      }))
-    } else return <h2>Loading</h2>
-  }
+  // renderCardCollection() {
+  //   if (this.props.cards) {
+  //     return (this.props.cards.map((card) => {
+  //         return (
+  //           <div className='card'>
+  //             <p>{card.name}</p>
+  //             <p>{card.class}</p>
+  //           </div>
+  //         )
+  //     }))
+  //   } else return <h2>Loading</h2>
+  // }
   
   render() {
     return (
-      <div className='dashboard-contents'>
-        {this.renderCardCollection()}
-      </div>
+      <Router>
+        <div className='dashboard-contents'>
+          {/* {this.renderCardCollection()} */}
+          {/* <Route path='/user/card' render={() => <UserCards />} /> */}
+          <UserCards />
+        </div>
+      </Router>
     )
   }
 }

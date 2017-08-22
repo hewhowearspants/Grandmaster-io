ddd1b5a386436381c0fb483c89f90e8244f54c50
dan martin <dmartss13@gmail.com>
2017-08-22 18:05:46 -0400

    accepting merge

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 
+import UserCards from './UserCards';
+
 class Card extends Component {
   render() {
     return (
+<<<<<<< HEAD
       <div className='Card'>
       <div className={`card ${this.props.card.class}`}>
         <div className='card-name'><b>{this.props.card.name}</b>
@@ -12,6 +15,13 @@ class Card extends Component {
           <p>Defense {this.props.card.defense}</p>
         </div>
       </div>
+=======
+      <div className='card' deleteCard={this.props.deleteCard} style={{backgroundImage: `url(${this.props.card.image_url})`}}>
+        <p>{this.props.card.name}</p>
+        <p>{this.props.card.class}</p>
+        <p>{this.props.card.attack}</p>
+        <p>{this.props.card.defense}</p>
+>>>>>>> delete card to card
       </div>
     )
   }

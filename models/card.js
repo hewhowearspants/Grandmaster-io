e37f83c776feac5211727a68541bd90cf2b0285f
Mark Ma <mazheng1022@hotmail.com>
2017-08-21 23:55:36 -0400

    add model find random ten cards and add cards to user table

--- models/card.js
+++ models/card.js
@@ -6,4 +6,22 @@ Card.findAll = () => {
   return db.query('SELECT * FROM cards');
 }
 
+Card.findTen = () => {
+  return db.query(`
+  SELECT * FROM cards
+  WHERE id > 0
+  ORDER BY RANDOM()
+  LIMIT 10
+  `);
+}
+
+Card.addToUser = (card,userid) => {
+  return db.one(`
+  INSERT INTO users_cards
+  (name, class, attack, defense, user_id)
+  VALUES ($1, $2, $3, $4, $5)
+  RETURNING *
+  `, [card.name, card.class, card.attack, card.defense, userid]);
+}
+
 module.exports = Card;

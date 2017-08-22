3897010da2ee30ea83551b1b8370955f4393e5c5
Mark Ma <mazheng1022@hotmail.com>
2017-08-22 12:42:02 -0400

    remove useless routes

--- client/src/App.js
+++ client/src/App.js
@@ -144,10 +144,7 @@ class App extends Component {
           <Route exact path='/' render={() => <Home handleLoginSubmit={this.handleLoginSubmit} />} />
           <Route exact path='/register' render={() => <Register handleRegisterSubmit={this.handleRegisterSubmit} />} />
           <Route exact path='/user' render={() => <Dashboard cards={this.state.cardData} />} />
-          {/* <Route exact path='/user/new' render={()=><NewUser />} /> */}
           {this.state.fireRedirect ? <Redirect push to={'/user'} /> : '' }
-          {/* <Route exact path='/register' render={()=>(this.state.isNewUser ? <NewUser jumpToUserHome={this.jumpToUserHome}/> : <Register handleRegisterSubmit={this.handleRegisterSubmit} />) }/> */}
-          {/* {this.state.isNewUser ? <Redirect push to={'/user/new'} /> : ''} */}
         </main>
         <Footer />
       </div>

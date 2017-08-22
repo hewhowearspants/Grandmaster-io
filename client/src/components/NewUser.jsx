16e5767bd6e6cc244546883d5418d029f9f2884f
Mark Ma <mazheng1022@hotmail.com>
2017-08-21 23:58:05 -0400

    add new component NewUser as the component of picking and saving random ten cards when new user registered

---
+++ client/src/components/NewUser.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class NewUser extends Component{
+    constructor(){
+        super();
+        this.state={
+            cardData:null,
+        }
+    }
+
+    componentDidMount(){
+        axios.get('/user/new')
+        .then(res => {
+            console.log(res.data)
+            this.setState({
+                cardData:res.data
+            })
+        })
+        .then(()=>{
+            this.state.cardData.map((data)=>{
+                axios.post('/user/new',{
+                    name:data.name,
+                    class:data.class,
+                    attack:data.attack,
+                    defense:data.defense
+                })
+                .then(res=>{
+                    console.log(res)
+                })
+                .catch(err=>{
+                    console.log(err)
+                })
+            })
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    }
+
+    render(){
+        return(
+            <div className='card-list'>
+                {this.state.cardData ?
+                    this.state.cardData.map((data)=>{
+                    return <div key={data.id} className='single-card'>
+                              <div className='card-name'><b>{data.name}</b></div>
+                              <div className='card-class'><p>class: {data.class}</p></div>
+                              <div className='card-attack'><p>attack: {data.attack}</p></div>
+                              <div className='card-defense'><p>defense: {data.defense}</p></div>
+                            </div>
+                }) : ''
+                }
+                <button className='userBtn' onClick={this.props.jumpToUserHome}>Accept!</button>
+             </div>
+        )
+    }
+}
+
+export default NewUser;

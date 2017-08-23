23156111dc548db5070efd748dee6fac26020aac
dan martin <dmartss13@gmail.com>
2017-08-23 11:50:30 -0400

    passing props to dashboard contents

--- client/src/components/Dashboard-Contents.jsx
+++ client/src/components/Dashboard-Contents.jsx
@@ -10,7 +10,7 @@ class DashboardContents extends Component {
   render() {
     return (
       <div className='dashboard-contents'>
-        {this.props.currentContent === 'user-cards' ? <UserCards userCards={this.props.userCards} /> : ''}
+        {this.props.currentContent === 'user-cards' ? <UserCards userSubmitEdit={this.props.userSubmitEdit} userSelectedCardToEdit={this.props.userSelectedCardToEdit} currentCardId={this.props.currentCardId} userCards={this.props.userCards} /> : ''}
         {this.props.currentContent === 'card-collection' ? <CardCollection cards={this.props.cards} /> : ''}
         {this.props.currentContent === 'get-new-card' ? <NewCard /> : ''}
         {this.props.currentcontent === 'leaderboard' ? <Leaderboard /> : ''}

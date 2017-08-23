98322296690160e634b0871aae44bcfaecaad22c
dan martin <dmartss13@gmail.com>
2017-08-23 18:17:05 -0400

    update user controller

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,38 +1,50 @@
-const bcrypt=require('bcryptjs');
-const User=require('../models/user.js');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user.js');
 
-const usersController={};
+const usersController = {};
 
-usersController.create=(req,res)=>{
-    const salt=bcrypt.genSaltSync();
-    const hash=bcrypt.hashSync(req.body.password,salt);
+usersController.create = (req,res) => {
+    const salt = bcrypt.genSaltSync();
+    const hash = bcrypt.hashSync(req.body.password,salt);
     User.create({
-        username:req.body.username,
-        password_digest:hash,
-        displayName:req.body.displayName,
-        email:req.body.email,
+        username: req.body.username,
+        password_digest: hash,
+        displayName: req.body.displayName,
+        email: req.body.email,
     })
-    .then(user=>{
-        req.login(user,(err)=>{
+    .then(user => {
+        req.login(user,(err) => {
             if(err)return next(err);
             res.json({
-                message:'ok',
-                user:user,
-                auth:true,
+                message: 'ok',
+                user: user,
+                auth: true,
             })
         });
     })
-    .catch(err=>{
+    .catch(err => {
         console.log(err);
         res.status(500).json(err);
     });
 }
 
-usersController.index=(req,res)=>{
+usersController.index = (req,res) => {
     res.json({
-        user:req.user,
-        data:'Put a user profile on this route'
+        user: req.user,
+        data: 'Put a user profile on this route'
     });
 }
 
-module.exports=usersController;
+usersController.update = (req, res) => {
+  console.log(req.params);
+  User.update(req.body.displayName, req.body.email, req.params.id)
+  .then(user => {
+    res.json(user);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+}
+
+module.exports = usersController;

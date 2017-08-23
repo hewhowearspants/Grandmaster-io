c16b1aadc2b4a438c830826e9a8dbbf39578bf29
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-08-23 12:21:50 -0400

    move delete functionality to delete icon

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -16,13 +16,12 @@ class Card extends Component {
               <p>{this.props.card.class}</p>
           </div>
           <div className='delete-button'>
-            <i className="fa fa-times"></i>
+            <i className="fa fa-times" onClick={()=> {this.props.deleteUserCard(this.props.card.id)}}></i>
           </div>
         </div>
         <div className='card-numbers'><p>ATT  {this.props.card.attack}</p>
           <p>DEF  {this.props.card.defense}</p>
         </div>
-        <button className='DeleteCard' onClick={()=> {this.props.deleteCard(this.props.card.id)}}>Delete!</button>
       </div>
       </div>
     )

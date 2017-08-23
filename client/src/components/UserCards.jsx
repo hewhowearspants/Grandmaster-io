58b3875346ce6c8b25b901d64b74058308fb9f62
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-08-23 12:24:36 -0400

    remove delete card function from user cards component - now in App.js

--- client/src/components/UserCards.jsx
+++ client/src/components/UserCards.jsx
@@ -1,30 +1,7 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import Card from './Card';
 
 class UserCards extends Component { 
-
-  deleteCard(id) {
-    axios.delete(`/usercard/${id}`)
-      .then(res => {
-        const updatedCards = [...this.state.userCardData];
-        console.log(updatedCards);
-        let deletedIndex;
-        updatedCards.forEach((card, index) => {
-          // console.log(card, id);
-          if (card.id === id) {
-            deletedIndex = index;
-          };
-        });
-        updatedCards.splice(deletedIndex, 1)
-        this.setState({
-          userCardData: updatedCards,
-        });
-      }).catch(err => {
-        console.log(err);
-      });
-  }
-
   render() {
     return (
       <div className='UserCards'>

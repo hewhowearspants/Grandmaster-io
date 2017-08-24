293be5a819a2245390b5920fe31207fa49ee57b6
Mark Ma <mazheng1022@hotmail.com>
2017-08-24 11:17:34 -0400

    not really using this ChatBox component, saving it for future use...maybe...

---
+++ client/src/components/ChatBox.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class ChatBox extends Component {
+    constructor() {
+        super();
+        this.state = {
+            text: '',
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        event.preventDefault();
+        this.setState({
+            text: event.target.value,
+        })
+    }
+
+    render(){
+        return(
+            <div className='message-box'>
+                <div className='message-display'>
+                    {this.props.messages.map((message)=>{
+                        return <p>{message.message}</p>
+                        })}
+                </div>
+                <div className='message-input'>
+                    <form onSubmit={()=>this.props.handleMessageSubmit(this.state.text)}>
+                        <input type='text' onChange={this.handleInputChange} />
+                        <button type='submit'>Send!</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ChatBox;

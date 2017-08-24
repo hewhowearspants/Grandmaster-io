4efb66e00ac2766e7a796e82769442dcd5a35c9f
dan martin <dmartss13@gmail.com>
2017-08-24 16:21:10 -0400

    ternary for button

--- client/src/components/UsersHands.jsx
+++ client/src/components/UsersHands.jsx
@@ -2,27 +2,13 @@ import React, { Component } from 'react';
 import HandCardSingle from './HandCardSingle';
 
 class UsersHands extends Component {
-    constructor() {
-        super();
-        this.state = {
-            joined: false,
-        }
-        this.joinGame = this.joinGame.bind(this);
-    }
-
-    joinGame() {
-        this.setState({
-            joined: true,
-        })
-    }
-
     render() {
         return (
             <div>
-                {this.state.joined ?
+                {this.props.joined ?
                     this.props.data.map(data => {
                         return <HandCardSingle select = {() => this.props.select(data)} key = {this.props.data.indexOf(data)} card = {data} makeSelection = {this.makeSelection} cardDrawn = {this.props.cardDrawn} />
-                    }) : <button onClick = {this.joinGame}>Join Game!</button>
+                    }) : <button onClick = {this.props.joinGame} disabled = {this.props.playersFull ? 'true' : 'false' }>Join Game!</button>
                 }
             </div>
         )

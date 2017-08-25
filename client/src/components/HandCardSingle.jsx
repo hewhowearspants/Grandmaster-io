62b77987646fe27c8c97c6cd826171503dc5771b
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-08-25 13:47:04 -0400

    only show card info if you have it

--- client/src/components/HandCardSingle.jsx
+++ client/src/components/HandCardSingle.jsx
@@ -20,7 +20,7 @@ class HandCardSingle extends Component {
 
     render(){
         return (
-        <div className = {`card ${this.props.card.class} ${this.state.displayNone ? 'hidden' : ''}`} onClick = {() => this.props.select(this.props.card) & this.setDisplay()}>
+        <div className = {`card ${this.props.card.class} ${this.state.displayNone ? 'hidden' : ''}`} onClick = {() => this.props.select(this.props.card) & this.setDisplay()} style={{background: `url(${this.props.card.image_url}`, backgroundSize: 'cover'}}>
             <div className='card-top'>
                 <div className='card-name'>
                     <b>{this.props.card.name}</b>
@@ -28,8 +28,8 @@ class HandCardSingle extends Component {
                 </div>
             </div>
             <div className='card-numbers'>
-                <p>ATT {this.props.card.attack}</p>
-                <p>DEF {this.props.card.defense}</p>
+                <p>{this.props.card.attack ? `ATT ${this.props.card.attack}` : ''}</p>
+                <p>{this.props.card.defense ? `DEF ${this.props.card.defense}` : ''}</p>
             </div>
         </div>
         )

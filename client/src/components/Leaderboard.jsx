cb29c75ebc9ce244a07f45dcec1d0b53dcd85972
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-08-30 12:56:23 -0400

    leaderboard info only maps if there is leaderboard info to map

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -10,25 +10,25 @@ class Leaderboard extends Component {
     // }
 
     componentDidMount(){
-        console.log(this.props.leaderInfo)
+        // console.log(this.props.leaderInfo)
     }
 
     render(){
         return(
             <div className = 'leader-board'>
                 <h3>Leaderboard</h3>
-                {this.props.leaderInfo.map(data => {
+                {this.props.leaderInfo ? this.props.leaderInfo.map(data => {
                     return <div className = 'leader-single' key = {data.id}>
                                 <h4>{this.props.leaderInfo.indexOf(data)+1}</h4>
                                 <div className = 'leader-start'>
-                                <div className = 'inside-leaderboard' style = {{ width: (data.wins+1)*10+'vw', background:`rgba(${Math.round(Math.random()*255)},${Math.round(Math.random()*255)},${Math.round(Math.random()*255)},1)`, animation: 'scoregrow 1s linear'}}>
-                                    <b>{data.display_name}</b>
-                                    <p style = {{color:'white'}}>Wins: {data.wins}</p>
-                                </div>
+                                    <b>{data.username} <span>({data.display_name})</span></b>
+                                    <div className = 'inside-leaderboard' style = {{ width: (data.wins+1)*10+'vw', background:`rgba(${data.wins*20},${Math.round(Math.random()*0)},${Math.round(Math.random()*0)},1)`, animation: 'scoregrow 1s linear'}}>
+                                        <p style = {{color:'white'}}>Wins: {data.wins}</p>
+                                    </div>
                                 </div>
                            </div>
                     })
-                }
+                : ''}
             </div>
         )
     }

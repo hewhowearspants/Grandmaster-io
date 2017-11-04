3733bc8a7dc23e2650966877c5a565071ed0adb6
Dan Martin <dmartss13@gmail.com>
2017-11-04 13:27:30 -0400

    dashboard contents stateless

--- client/src/components/Dashboard-Contents.jsx
+++ client/src/components/Dashboard-Contents.jsx
@@ -1,36 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import UserCards from './UserCards';
-import CardCollection from './CardCollection';
-import NewCard from './NewCard';
-import Leaderboard from './Leaderboard';
-import UserProfile from './UserProfile';
+import UserCards from "./UserCards";
+import { n } from "./CardCollection";
+import NewCard from "./NewCard";
+import Leaderboard from "./Leaderboard";
+import { CardCollection } from "./CardCollection";
+import UserProfile from "./UserProfile";
 
 // the container for all of the pages in the user dashboard
 // changes based on the currentContent state in App.js
-class DashboardContents extends Component {  
-  
-  render() {
-    return (
-      <div className='dashboard-contents'>
-        {this.props.currentContent === 'user-cards' ? <UserCards userCards = {this.props.userCards} 
-                                                        deleteUserCard = {this.props.deleteUserCard}
-                                                        userSubmitEdit = {this.props.userSubmitEdit}
-                                                        userSelectedCardToEdit = {this.props.userSelectedCardToEdit}
-                                                        currentCardId = {this.props.currentCardId}/> : ''}
-        {this.props.currentContent === 'card-collection' ? <CardCollection cards = {this.props.cards} /> : ''}
-        {this.props.currentContent === 'get-new-card' ? <NewCard newCard = {this.props.newCard} getNewUserCard = {this.props.getNewUserCard} getNewUserCardPremium = {this.props.getNewUserCardPremium} userCards={this.props.userCards} /> : ''}
-        {this.props.currentContent === 'user-profile' ? <UserProfile userSubmitNewName = {this.props.userSubmitNewName}
-                                                                    userSelectedNameToEdit = {this.props.userSelectedNameToEdit} 
-                                                                    currentUserId = {this.props.currentUserId}
-                                                                    user = {this.props.user}
-                                                                    email = {this.props.email}
-                                                                    display_name = {this.props.display_name}
-                                                                    deleteUser = {this.props.deleteUser} /> : ''}
-        {this.props.currentContent === 'leaderboard' ? <Leaderboard leaderInfo = {this.props.leaderInfo} /> : ''}
-      </div>
-    )
-  }
-}
+const DashboardContents = ({
+  deleteUserCard,
+  userSubmitEdit,
+  userSelectedCardToEdit,
+  currentCardId,
+  currentContent,
+  newCard,
+  getNewUserCard,
+  getNewUserCardPremium,
+  userCards,
+  userSubmitNewName,
+  userSelectedNameToEdit,
+  currentUserId,
+  cards,
+  user,
+  email,
+  display_name,
+  deleteUser,
+  leaderInfo
+}) => (
+  <div className="dashboard-contents">
+    {currentContent === "user-cards" ? (
+      <UserCards
+        userCards={userCards}
+        deleteUserCard={deleteUserCard}
+        userSubmitEdit={userSubmitEdit}
+        userSelectedCardToEdit={userSelectedCardToEdit}
+        currentCardId={currentCardId}
+      />
+    ) : currentContent === "card-collection" ? (
+      <CardCollection cards={cards} />
+    ) : currentContent === "get-new-card" ? (
+      <NewCard
+        newCard={newCard}
+        getNewUserCard={getNewUserCard}
+        getNewUserCardPremium={getNewUserCardPremium}
+        userCards={userCards}
+      />
+    ) : currentContent === "user-profile" ? (
+      <UserProfile
+        userSubmitNewName={userSubmitNewName}
+        userSelectedNameToEdit={userSelectedNameToEdit}
+        currentUserId={currentUserId}
+        user={user}
+        email={email}
+        display_name={display_name}
+        deleteUser={deleteUser}
+      />
+    ) : currentContent === "leaderboard" ? (
+      <Leaderboard leaderInfo={leaderInfo} />
+    ) : null}
+  </div>
+);
 
 export default DashboardContents;

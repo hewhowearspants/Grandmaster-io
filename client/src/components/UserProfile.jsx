c72f6c0c0e358b0468e9e48ebe80e97b21d07d9b
Dan Martin <dmartss13@gmail.com>
2017-11-04 13:48:39 -0400

    cleanup user profile

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,51 +1,86 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // displays the users profile info in the dashboard
 // allows the user to edit or delete their profile info
 class UserProfile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      display_name: this.props.user.display_name,
-      email: this.props.user.email,
-      id: this.props.user.id,
-    }
-  }
+  state = {
+    display_name: this.props.user.display_name,
+    email: this.props.user.email,
+    id: this.props.user.id
+  };
 
   // handles input change for the profile info edit form
-  handleInputChange = (e) => {
+  handleInputChange = e => {
     const name = e.target.name;
     const value = e.target.value;
     console.log(this.state.id);
     console.log(e.target.value);
     this.setState({
-        [name]: value,
+      [name]: value
     });
-  }
+  };
 
   render() {
+    const {
+      user,
+      currentUserId,
+      userSelectedNameToEdit,
+      deleteUser,
+      userSubmitNewName
+    } = this.props;
     return (
-      <div className = 'user-profile'>
-        <div className = 'user'>
-          {this.props.currentUserId === this.props.user.id ?
-            <form onSubmit = {this.props.userSubmitNewName}>
-              <input type = 'text' name = 'display_name' placeholder = 'username' value = {this.state.display_name} onChange = {this.handleInputChange} />
-              <input type = 'text' name = 'email' placeholder = 'email' value = {this.state.email} onChange = {this.handleInputChange} />
-              <button type = 'submit'>Submit</button>
-            </form> 
-            : <h2><span>username</span>{this.props.user.display_name} <span>email</span>{this.props.user.email}</h2> }
+      <div className="user-profile">
+        <div className="user">
+          {currentUserId === user.id ? (
+            <form onSubmit={userSubmitNewName}>
+              <input
+                type="text"
+                name="display_name"
+                placeholder="username"
+                value={this.state.display_name}
+                onChange={this.handleInputChange}
+              />
+              <input
+                type="text"
+                name="email"
+                placeholder="email"
+                value={this.state.email}
+                onChange={this.handleInputChange}
+              />
+              <button type="submit">Submit</button>
+            </form>
+          ) : (
+            <h2>
+              <span>username</span>
+              {user.display_name} <span>email</span>
+              {user.email}
+            </h2>
+          )}
         </div>
-        <div className='user-buttons'>
-          <div className='edit-button'>
-            <i className="fa fa-pencil fa-2x" onClick={()=> {this.props.userSelectedNameToEdit(this.props.user.id)}}><p>Edit Profile?</p></i>
+        <div className="user-buttons">
+          <div className="edit-button">
+            <i
+              className="fa fa-pencil fa-2x"
+              onClick={() => {
+                userSelectedNameToEdit(user.id);
+              }}
+            >
+              <p>Edit Profile?</p>
+            </i>
           </div>
-          <div className='delete-button'>
-            <i className="fa fa-times fa-2x" onClick={()=> {this.props.deleteUser(this.props.user.id) }}><p>Delete Profile?</p></i>
+          <div className="delete-button">
+            <i
+              className="fa fa-times fa-2x"
+              onClick={() => {
+                deleteUser(user.id);
+              }}
+            >
+              <p>Delete Profile?</p>
+            </i>
           </div>
         </div>
-
       </div>
-    )
+    );
   }
 }
 

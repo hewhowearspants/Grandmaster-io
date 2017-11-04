3eb8ddf8d73e3ba882e880d61bc1700004f47f33
Dan Martin <dmartss13@gmail.com>
2017-11-04 14:39:53 -0400

    clean up home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import Login from './Login';
+import Login from "./Login";
 
 // the login screen! user redirects to here if not logged in
-const Home = (props) => {
-    return (
-        <div className = 'home'>
-            <h1>Welcome, Grandmaster</h1>
-            <ul>
-                <li><Login handleLoginSubmit = {props.handleLoginSubmit} /></li>
-                <li>Not logged in? <Link to = '/register'>Register</Link></li>
-            </ul>
-        </div>
-    )
-}
-
-export default Home;
+export const Home = props => (
+  <div className="home">
+    <h1>Welcome, Grandmaster</h1>
+    <ul>
+      <li>
+        <Login handleLoginSubmit={props.handleLoginSubmit} />
+      </li>
+      <li>
+        Not logged in? <Link to="/register">Register</Link>
+      </li>
+    </ul>
+  </div>
+);

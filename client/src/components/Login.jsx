8b2cd47da1908b83196d1a50c7b854652e666848
Dan Martin <dmartss13@gmail.com>
2017-11-04 13:48:10 -0400

    cleanup login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,34 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // the login component in the Home component
 class Login extends Component {
-    constructor () {
-        super();
-        this.state = {
-            username: '',
-            password: '',
-        }
-    }
+  state = {
+    username: "",
+    password: ""
+  };
 
-    handleInputChange = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        this.setState({
-            [name]: value,
-        });
-    }
+  handleInputChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
 
-    render() {
-        return (
-            <div className = 'login'>
-                <form onSubmit = {(e) => this.props.handleLoginSubmit(e, this.state.username, this.state.password)}>
-                    <input type = 'text' name = 'username' value = {this.state.username} placeholder = 'Username' onChange = {this.handleInputChange} />
-                    <input type = 'password' name = 'password' value = {this.state.password} placeholder = 'Password' onChange = {this.handleInputChange} />
-                    <button type = 'submit'>Log in!</button>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    const { username, password } = this.state;
+    const { handleLoginSubmit } = this.props;
+    return (
+      <div className="login">
+        <form onSubmit={e => handleLoginSubmit(e, username, password)}>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            placeholder="Username"
+            onChange={this.handleInputChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Password"
+            onChange={this.handleInputChange}
+          />
+          <button type="submit">Log in!</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login;

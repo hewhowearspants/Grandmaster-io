2007dc6ed61a5afa839167cbf8c8a677e0847186
Dan Martin <dmartss13@gmail.com>
2017-11-04 12:05:55 -0400

    for setting up local

--- db/config.js
+++ db/config.js
@@ -1,21 +1,21 @@
 const options = {
-    query:(e) => {
-        console.log(e.query);
-    }
+  query: e => {
+    console.log(e.query);
+  }
 };
 
-const pgp=require('pg-promise')(options);
+const pgp = require("pg-promise")(options);
 
-function setDatabase(){
-    if(process.env.NODE_ENV==='development'||!process.env.NODE_ENV){
-        return pgp({
-            database: 'dream_team_dev',
-            port: 5432,
-            host: 'localhost',
-        })
-    } else {
-        return pgp(process.env.DATABASE_URL);
-    }
+function setDatabase() {
+  if (process.env.NODE_ENV === "development" || !process.env.NODE_ENV) {
+    return pgp({
+      database: "dream_team_dev",
+      port: 5432,
+      host: "localhost"
+    });
+  } else {
+    return pgp(process.env.DATABASE_URL);
+  }
 }
 
 const db = setDatabase();

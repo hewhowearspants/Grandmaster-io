4850f4803130e2c48f49d181f30445657a5a94a5
Dan Martin <dmartss13@gmail.com>
2017-11-04 13:46:56 -0400

    leaderboard to stateless

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -1,37 +1,34 @@
-import React, { Component } from 'react';
+import React from "react";
 
 // displays all users by how many wins they have
-class Leaderboard extends Component {
-    // constructor(){
-    //     super();
-    //     this.state=({
-    //         leaderInfo: null
-    //     })
-    // }
-
-    componentDidMount(){
-        // console.log(this.props.leaderInfo)
-    }
-
-    render(){
-        return(
-            <div className = 'leader-board'>
-                <h3>Leaderboard</h3>
-                {this.props.leaderInfo ? this.props.leaderInfo.map(data => {
-                    return <div className = 'leader-single' key = {data.id}>
-                                <h4>{this.props.leaderInfo.indexOf(data)+1}</h4>
-                                <div className = 'leader-start'>
-                                    <b>{data.username} <span>({data.display_name})</span></b>
-                                    <div className = 'inside-leaderboard' style = {{ width: (data.wins+1)*2+'vw', background:`rgba(${data.wins*20},${Math.round(Math.random()*0)},${Math.round(Math.random()*0)},1)`, animation: 'scoregrow 1s linear'}}>
-                                        <p style = {{color:'white'}}>Wins: {data.wins}</p>
-                                    </div>
-                                </div>
-                           </div>
-                    })
-                : ''}
+export const Leaderboard = ({ leaderInfo }) => (
+  <div className="leader-board">
+    <h3>Leaderboard</h3>
+    {leaderInfo
+      ? leaderInfo.map(data => {
+          return (
+            <div className="leader-single" key={data.id}>
+              <h4>{leaderInfo.indexOf(data) + 1}</h4>
+              <div className="leader-start">
+                <b>
+                  {data.username} <span>({data.display_name})</span>
+                </b>
+                <div
+                  className="inside-leaderboard"
+                  style={{
+                    width: (data.wins + 1) * 2 + "vw",
+                    background: `rgba(${data.wins * 20},${Math.round(
+                      Math.random() * 0
+                    )},${Math.round(Math.random() * 0)},1)`,
+                    animation: "scoregrow 1s linear"
+                  }}
+                >
+                  <p style={{ color: "white" }}>Wins: {data.wins}</p>
+                </div>
+              </div>
             </div>
-        )
-    }
-}
-
-export default Leaderboard;
+          );
+        })
+      : ""}
+  </div>
+);

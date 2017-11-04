f3be06a68b3edb904cdb81de13eb5861ecb1319c
Dan Martin <dmartss13@gmail.com>
2017-11-04 13:47:24 -0400

    dashboard nav to stateless

--- client/src/components/Dashboard-Nav.jsx
+++ client/src/components/Dashboard-Nav.jsx
@@ -1,22 +1,41 @@
-import React, { Component } from 'react';
+import React from "react";
 
 // the nav component of the dashboard, sets the currentContent in App.js,
 // which sets which content component to display
-class DashboardNav extends Component {
-  render() {
-    return (
-      <div className = 'dashboard-nav'>
-        <ul>
-          <h1>Dashboard</h1>
-          <li className = {this.props.currentContent === 'user-cards' ? 'selected' : ''} onClick = {() => this.props.setContent('user-cards')}>Your Cards</li>
-          <li className = {this.props.currentContent === 'card-collection' ? 'selected' : ''} onClick = {() => this.props.setContent('card-collection')}>Collection</li>
-          <li className = {this.props.currentContent === 'get-new-card' ? 'selected' : ''} onClick = {() => this.props.setContent('get-new-card')}>Get New Card</li>
-          <li className = {this.props.currentContent === 'leaderboard' ? 'selected' : ''} onClick = {() => this.props.setContent('leaderboard')}>Leaderboard</li>
-          <li className = {this.props.currentContent === 'user-profile' ? 'selected' : ''} onClick = {() => this.props.setContent('user-profile')}>Your Profile</li>
-        </ul>
-      </div>
-    )
-  }
-}
-
-export default DashboardNav;
+export const DashboardNav = ({ currentContent, setContent }) => (
+  <div className="dashboard-nav">
+    <ul>
+      <h1>Dashboard</h1>
+      <li
+        className={currentContent === "user-cards" ? "selected" : ""}
+        onClick={() => setContent("user-cards")}
+      >
+        Your Cards
+      </li>
+      <li
+        className={currentContent === "card-collection" ? "selected" : ""}
+        onClick={() => setContent("card-collection")}
+      >
+        Collection
+      </li>
+      <li
+        className={currentContent === "get-new-card" ? "selected" : ""}
+        onClick={() => setContent("get-new-card")}
+      >
+        Get New Card
+      </li>
+      <li
+        className={currentContent === "leaderboard" ? "selected" : ""}
+        onClick={() => setContent("leaderboard")}
+      >
+        Leaderboard
+      </li>
+      <li
+        className={currentContent === "user-profile" ? "selected" : ""}
+        onClick={() => setContent("user-profile")}
+      >
+        Your Profile
+      </li>
+    </ul>
+  </div>
+);

b3dcaa42e9e269333e85cd0a5bb480e4e8737da3
Dan Martin <dmartss13@gmail.com>
2017-11-04 14:41:05 -0400

    organize header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,20 +11,20 @@ export const Header = props => (
         <li className="username">{props.user.display_name}</li>
         <li className="username">Wins: {props.user.wins}</li>
         <li className="username">Currency: {props.user.currency}</li>
+        {props.currentPage === "game" || !props.auth ? null : (
+          <li onClick={() => props.setCurrentPage("game")}>
+            <Link to="/joingame">Join Game</Link>
+          </li>
+        )}
+        {props.currentPage === "dashboard" || !props.auth ? null : (
+          <li onClick={() => props.setCurrentPage("dashboard")}>
+            <Link to="/user">User Dashboard</Link>
+          </li>
+        )}
         <li onClick={props.logOut}>
           <Link to="/">Logout</Link>
         </li>
       </ul>
     ) : null}
-    {props.currentPage === "game" || !props.auth ? null : (
-      <li onClick={() => props.setCurrentPage("game")}>
-        <Link to="/joingame">Join Game</Link>
-      </li>
-    )}
-    {props.currentPage === "dashboard" || !props.auth ? null : (
-      <li onClick={() => props.setCurrentPage("dashboard")}>
-        <Link to="/user">User Dashboard</Link>
-      </li>
-    )}
   </header>
 );

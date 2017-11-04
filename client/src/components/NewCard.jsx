43b0e3a8767e94f4af94772486aac2661db4ca28
Dan Martin <dmartss13@gmail.com>
2017-11-04 12:50:47 -0400

    new card ternarys and code cleanup

--- client/src/components/NewCard.jsx
+++ client/src/components/NewCard.jsx
@@ -1,89 +1,109 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Card from './Card';
+import Card from "./Card";
 
+const alert = `Too many cards! You can't have more than 20 cards.`;
 
 // gets a new card when the user clicks a button
 class NewCard extends Component {
-    constructor() {
-        super();
-        this.state = {
-            gotNewCard: false,
-        }
-    }
+  state = { gotNewCard: false };
 
-    componentWillUnmount() {
-        this.setState({
-            gotNewCard: false,
-        })
-    }
+  componentWillUnmount() {
+    this.setState({
+      gotNewCard: false
+    });
+  }
 
-    getNewCard = () => {
-        if(this.props.userCards.length < 20){
-            this.props.getNewUserCard();
-            this.setState({
-                gotNewCard: true,
-            })
-        }else{
-            alert('Too many cards! You can\'t have more than 20 cards.')
-        }
-    }
+  getNewCard = () => {
+    this.props.userCards.length > 20
+      ? alert(`${alert}`)
+      : this.props.getNewUserCard();
+    this.setState({
+      gotNewCard: true
+    });
+  };
 
-    getNewCardLv1 = () => {
-        if(this.props.userCards.length < 20){
-            this.props.getNewUserCardPremium(15);
-            this.setState({
-                gotNewCard: true,
-            })
-        }else{
-            alert('Too many cards! You can\'t have more than 20 cards.')
-        }
-    }
+  getNewCardLv1 = () => {
+    this.props.userCards.length > 20
+      ? alert(`${alert}`)
+      : this.props.getNewUserCardPremium(15);
+    this.setState({
+      gotNewCard: true
+    });
+  };
 
-    getNewCardLv2 = () => {
-        if(this.props.userCards.length < 20){
-            this.props.getNewUserCardPremium(25);
-            this.setState({
-                gotNewCard: true,
-            })
-        }else{
-            alert('Too many cards! You can\'t have more than 20 cards.')
-        }
-    }
+  getNewCardLv2 = () => {
+    this.props.userCards.length > 20
+      ? alert(`${alert}`)
+      : this.props.getNewUserCardPremium(25);
+    this.setState({
+      gotNewCard: true
+    });
+  };
 
-    getNewCardLv3 = () => {
-        if(this.props.userCards.length < 20){
-            this.props.getNewUserCardPremium(35);
-            this.setState({
-                gotNewCard: true,
-            })
-        }else{
-            alert('Too many cards! You can\'t have more than 20 cards.')
-        }
-    }
+  getNewCardLv3 = () => {
+    this.props.userCards.length > 20
+      ? alert(`${alert}`)
+      : this.props.getNewUserCardPremium(35);
+    this.setState({
+      gotNewCard: true
+    });
+  };
 
-    getNewCardLv4 = () => {
-        if(this.props.userCards.length < 20){
-            this.props.getNewUserCardPremium(45);
-            this.setState({
-                gotNewCard: true,
-            })
-        }else{
-            alert('Too many cards! You can\'t have more than 20 cards.')
-        }
-    }
+  getNewCardLv4 = () => {
+    this.props.userCards.length > 20
+      ? alert(`${alert}`)
+      : this.props.getNewUserCardPremium(45);
+    this.setState({
+      gotNewCard: true
+    });
+  };
 
   render() {
     return (
-      <div className = 'new-card'>
-          <button className = 'newCardButton' type='button' onClick = {this.getNewCard}>Get new Card! Cost: <strong style={{color:'red'}}>20</strong></button>
-          <button className = 'newCardButton' type='button' onClick = {this.getNewCardLv1}>Get better Card! Cost: <strong style={{color:'red'}}>30</strong></button>
-          <button className = 'newCardButton' type='button' onClick = {this.getNewCardLv2}>Get even better Card! Cost: <strong style={{color:'red'}}>50</strong></button>
-          <button className = 'newCardButton' type='button' onClick = {this.getNewCardLv3}>Get almost the best Card! Cost: <strong style={{color:'red'}}>70</strong></button>
-          <button className = 'newCardButton' type='button' onClick = {this.getNewCardLv4}>Get the BEST Card! Cost: <strong style={{color:'red'}}>90</strong></button>
-        {this.state.gotNewCard && this.props.newCard ? <Card card = {this.props.newCard[0]} /> : ''}
+      <div className="new-card">
+        <button
+          className="newCardButton"
+          type="button"
+          onClick={this.getNewCard}
+        >
+          Get new Card! Cost: <strong style={{ color: "red" }}>20</strong>
+        </button>
+        <button
+          className="newCardButton"
+          type="button"
+          onClick={this.getNewCardLv1}
+        >
+          Get better Card! Cost: <strong style={{ color: "red" }}>30</strong>
+        </button>
+        <button
+          className="newCardButton"
+          type="button"
+          onClick={this.getNewCardLv2}
+        >
+          Get even better Card! Cost:
+          <strong style={{ color: "red" }}>50</strong>
+        </button>
+        <button
+          className="newCardButton"
+          type="button"
+          onClick={this.getNewCardLv3}
+        >
+          Get almost the best Card! Cost:
+          <strong style={{ color: "red" }}>70</strong>
+        </button>
+        <button
+          className="newCardButton"
+          type="button"
+          onClick={this.getNewCardLv4}
+        >
+          Get the BEST Card! Cost: <strong style={{ color: "red" }}>90</strong>
+        </button>
+        {this.state.gotNewCard && this.props.newCard ? (
+          <Card card={this.props.newCard[0]} />
+        ) : null}
       </div>
-    )
+    );
   }
 }
 

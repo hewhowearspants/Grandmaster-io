323e584d16955e7b6e550736510fff3eaeb2ff99
Dan Martin <dmartss13@gmail.com>
2017-11-15 09:32:16 -0500

    user controller to async await try catch

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,93 +1,93 @@
-const bcrypt = require('bcryptjs');
-const User = require('../models/user.js');
+const bcrypt = require("bcryptjs");
+const User = require("../models/user.js");
 
 const usersController = {};
 //create new user
-usersController.create = (req,res) => {
-    const salt = bcrypt.genSaltSync();
-    const hash = bcrypt.hashSync(req.body.password,salt);
-    User.create({
-        username: req.body.username,
-        password_digest: hash,
-        displayName: req.body.displayName,
-        email: req.body.email,
-    })
-    .then(user => {
-        req.login(user,(err) => {
-            if(err)return next(err);
-            res.json({
-                message: 'ok',
-                user: user,
-                auth: true,
-            })
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+usersController.create = async (req, res) => {
+  try {
+    const salt = await bcrypt.genSaltSync();
+    const hash = await bcrypt.hashSync(req.body.password, salt);
+    const user = await User.create({
+      username: req.body.username,
+      password_digest: hash,
+      displayName: req.body.displayName,
+      email: req.body.email
     });
-}
+    req.login(user, err => {
+      if (err) return next(err);
+      res.json({
+        message: "ok",
+        user: user,
+        auth: true
+      });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+};
 
-usersController.index = (req,res) => {
-    res.json({
-        user: req.user,
-        data: 'Put a user profile on this route'
-    });
-}
+usersController.index = async (req, res) => {
+  const user = await res.json({
+    user: req.user,
+    data: "Put a user profile on this route"
+  });
+};
 //edit user's info
-usersController.update = (req, res) => {
+usersController.update = async (req, res) => {
+  try {
     console.log(req.params);
-    User.update(req.body.displayName, req.body.email, req.params.id)
-    .then(user => {
-      res.json(user);
+    const user = await User.update(
+      req.body.displayName,
+      req.body.email,
+      req.params.id
+    );
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+};
+//delete user
+usersController.delete = async (req, res) => {
+  try {
+    const user = await User.destroy(req.params.id);
+    return res.json({
+      message: "ok",
+      user: user
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err });
+  }
+};
+//show leaderboard page
+usersController.showLeaderboard = (req, res) => {
+  User.showLeaderboard()
+    .then(users => {
+      res.json({
+        message: "ok",
+        data: users
+      });
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
-  }
-//delete user
-usersController.delete = (req, res) => {
-  User.destroy(req.params.id)
-  .then(user => {
-    res.json({
-      message: 'ok',
-      user: user,
-    })
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({ err });
-  })
-}
-//show leaderboard page
-usersController.showLeaderboard = (req, res) => {
-    User.showLeaderboard()
-    .then(users => {
-        res.json({
-            message: 'ok',
-            data: users
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
-}
+};
 //update currency and wins after player wins the battle
-usersController.updateCurrencyNWins = (req, res) => {
-    User.updateCurrencyNWins({
-        currency: req.body.currency,
-        wins: req.body.wins,
-        username: req.body.username,
-    })
-    .then(user => {
-        res.json(user);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
-}
+usersController.updateCurrencyNWins = async (req, res) => {
+  try {
+    const user = await User.updateCurrencyNWins({
+      currency: req.body.currency,
+      wins: req.body.wins,
+      username: req.body.username
+    });
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+};
 
-module.exports=usersController;
+module.exports = usersController;

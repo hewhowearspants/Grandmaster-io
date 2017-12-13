2917a8efec81a3f21b7eb93639a4b28a00b9349a
Dan Martin <dmartss13@gmail.com>
2017-12-13 00:18:14 -0500

    handle submit props

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,15 +5,15 @@ import { Link } from "react-router-dom";
 import Login from "./Login";
 
 // the login screen! user redirects to here if not logged in
-export const Home = props => (
+export const Home = ({ handleLoginSubmit }) => (
   <div className="home">
     <h1>Welcome, Grandmaster</h1>
     <ul>
       <li>
-        <Login handleLoginSubmit={props.handleLoginSubmit} />
+        <Login handleLoginSubmit={handleLoginSubmit} />
       </li>
       <li>
-        Not logged in? <Link to="/register">Register</Link>
+        Not Registered? <Link to="/register">Register</Link>
       </li>
     </ul>
   </div>

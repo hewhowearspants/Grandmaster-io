955515e5e3f85180e7fbd17f611612995be61933
Dan Martin <dmartss13@gmail.com>
2017-12-13 00:18:06 -0500

    user hand if

--- client/src/components/UsersHands.jsx
+++ client/src/components/UsersHands.jsx
@@ -4,17 +4,16 @@ import { HandCardSingle } from "./HandCardSingle";
 // the playing card container for the game board
 export const UsersHands = ({ cardData, cardDrawn, opponent, select }) => (
   <div>
-    {cardData
-      ? cardData.map(card => (
-          <HandCardSingle
-            select={() => select(card)}
-            key={cardData.indexOf(card)}
-            card={card}
-            makeSelection={this.makeSelection}
-            cardDrawn={cardDrawn}
-            opponent={opponent}
-          />
-        ))
-      : null}
+    {cardData &&
+      cardData.map(card => (
+        <HandCardSingle
+          select={() => select(card)}
+          key={cardData.indexOf(card)}
+          card={card}
+          makeSelection={this.makeSelection}
+          cardDrawn={cardDrawn}
+          opponent={opponent}
+        />
+      ))}
   </div>
 );

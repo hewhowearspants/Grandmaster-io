4cbba609738f34a2fdc758411fc0554abb7406d3
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-02-16 17:00:25 -0500

    give div inside UsersHands a class name

--- client/src/components/UsersHands.jsx
+++ client/src/components/UsersHands.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { HandCardSingle } from "./HandCardSingle";
 
 // the playing card container for the game board
-export const UsersHands = ({ cardData, cardDrawn, opponent, select }) => (
-  <div>
+export const UsersHands = ({ cardData, cardDrawn, opponent, select, className }) => (
+  <div className={`${className}`}>
     {cardData
       ? cardData.map(card => (
           <HandCardSingle

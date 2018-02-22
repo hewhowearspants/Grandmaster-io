f2237d7eca16431b87c68d40a8c91cb9879c668e
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-02-22 15:48:50 -0500

    add users_counters table, like users_cards table

---
+++ db/migrations/migration-022218.sql
@@ -0,0 +1,47 @@
+\c dream_team_dev;
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  password_digest TEXT NOT NULL,
+  display_name VARCHAR(255),
+  email VARCHAR(255),
+  wins INT,
+  currency INT
+);
+
+CREATE TABLE IF NOT EXISTS cards (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  class VARCHAR(255),
+  attack INTEGER,
+  defense INTEGER,
+  image_url VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS users_cards (
+  id SERIAL PRIMARY KEY,
+  card_id INTEGER REFERENCES cards(id),
+  name VARCHAR(255),
+  class VARCHAR(255),
+  attack INTEGER,
+  defense INTEGER,
+  image_url VARCHAR(255),
+  user_id INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS counter_cards (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  description TEXT,
+  usable_by TEXT[]
+);
+
+CREATE TABLE IF NOT EXISTS users_counters (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  description TEXT,
+  usable_by TEXT[],
+  card_id INTEGER REFERENCES counter_cards(id),
+  user_id INTEGER REFERENCES users(id)
+);

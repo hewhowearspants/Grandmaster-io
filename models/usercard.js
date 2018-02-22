0fd29f64af5c625f291e52da31e7f06f29a3c934
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-02-22 15:58:11 -0500

    add Counter functions to Usercard model, also ungoof formatting

--- models/usercard.js
+++ models/usercard.js
@@ -1,24 +1,22 @@
-const db = require("../db/config");
+const db = require('../db/config');
 
 const Usercard = {};
-//find all users' cards
-Usercard.findUserCards = userid =>
-  db.manyOrNone(
-    `
+
+// find all users' cards
+Usercard.findUserCards = (userId) =>
+  db.manyOrNone(`
     SELECT * FROM users_cards
     WHERE user_id = $1
     ORDER BY card_id, id ASC
-    `,
-    [userid]
-  );
-//add new card to user
-Usercard.addToUser = (card, userid) =>
-  db.one(
-    `
-  INSERT INTO users_cards
-  (card_id, name, class, attack, defense, image_url, user_id)
-  VALUES ($1, $2, $3, $4, $5, $6, $7)
-  RETURNING *
+  `, [userId]);
+
+// add new card to user
+Usercard.addToUser = (card, userId) =>
+  db.one(`
+    INSERT INTO users_cards
+    (card_id, name, class, attack, defense, image_url, user_id)
+    VALUES ($1, $2, $3, $4, $5, $6, $7)
+    RETURNING *
   `,
     [
       card.cardId,
@@ -27,39 +25,50 @@ Usercard.addToUser = (card, userid) =>
       card.attack,
       card.defense,
       card.imageUrl,
-      userid
+      userId
     ]
   );
-//edit card name
+
+// find all counter cards for a specified user
+Usercard.findUserCounters = (userId) =>
+  db.manyOrNone(`
+    SELECT * FROM users_counters
+    WHERE user_id = $1
+    ORDER BY card_id, id ASC
+  `, [userId]);
+
+// add counter card to users_counters
+Usercard.addCounter = (card, userId) =>
+  db.one(`
+    INSERT INTO users_counters
+    (name, description, usable_by, card_id, user_id)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *
+  `, [card.name, card.description, card.usableBy, card.cardId, userId]);
+
+// edit card name
 Usercard.update = (name, id) =>
-  db.one(
-    `
-  UPDATE users_cards SET
-  name = $1
-  WHERE id = $2
-  RETURNING *
-  `,
-    [name, id]
-  );
-//delete card
-Usercard.destroy = id =>
-  db.none(
-    `
-  DELETE FROM users_cards
-  WHERE id = $1
-  `,
-    [id]
-  );
-//get five random cards to prepare for the battle
-Usercard.findFiveUserCards = id =>
-  db.manyOrNone(
-    `
-  SELECT * FROM users_cards
-  WHERE user_id = $1
-  ORDER BY RANDOM()
-  LIMIT 5
-  `,
-    [id]
-  );
+  db.one(`
+    UPDATE users_cards SET
+    name = $1
+    WHERE id = $2
+    RETURNING *
+  `, [name, id]);
+
+// delete card
+Usercard.destroy = (id) =>
+  db.none(`
+    DELETE FROM users_cards
+    WHERE id = $1
+  `, [id]);
+
+// get five random cards to prepare for the battle
+Usercard.findFiveUserCards = (id) =>
+  db.manyOrNone(`
+    SELECT * FROM users_cards
+    WHERE user_id = $1
+    ORDER BY RANDOM()
+    LIMIT 5
+  `, [id]);
 
 module.exports = Usercard;

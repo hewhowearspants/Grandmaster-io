73d9563e544a71fc5be6a77e46d42f3b63bb48e4
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-02-22 15:56:12 -0500

    add findCounter function to Card model, ungoof styling

--- models/card.js
+++ models/card.js
@@ -1,32 +1,42 @@
-const db = require("../db/config");
+const db = require('../db/config');
 
 const Card = {};
-//Find all cards, for the card collection page
+
+// Find all cards, for the card collection page
 Card.findAll = () => db.query("SELECT * FROM cards");
-//Find random ten cards, for new users registration
+
+// Find random ten cards, for new users registration
 Card.findTen = () =>
   db.query(`
-  SELECT * FROM cards
-  ORDER BY RANDOM()
-  LIMIT 10
-  `);
-//Find random one card, for 'get new card'
-Card.findOne = () =>
-  db.query(`
-  SELECT * FROM cards
-  ORDER BY RANDOM()
-  LIMIT 1
+    SELECT * FROM cards
+    ORDER BY RANDOM()
+    LIMIT 10
   `);
 
-Card.findPremiumOne = num =>
-  db.query(
-    `
-  SELECT * FROM cards
-  where id > $1
-  ORDER BY RANDOM()
-  LIMIT 1
-  `,
-    [num]
-  );
+// Find random one card, for 'get new card'
+Card.findOne = () =>
+  db.query(`
+    SELECT * FROM cards
+    ORDER BY RANDOM()
+    LIMIT 1
+  `);
+
+// Find a random card within a limited range
+Card.findPremiumOne = (num) =>
+  db.query(`
+    SELECT * FROM cards
+    where id > $1
+    ORDER BY RANDOM()
+    LIMIT 1
+  `, [num]);
+
+// Select a random counter card
+Card.findCounter = () => 
+  db.query(`
+    SELECT * FROM counter_cards
+    ORDER BY RANDOM()
+    LIMIT 1
+  `);
+
 
 module.exports = Card;

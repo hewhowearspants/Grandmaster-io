fc2c44e10d601761340f48dcfb7173466b3a62dd
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-02-27 19:13:55 -0500

    update CardCollection component to be able to receive different types of cards

--- client/src/components/Dashboard-Contents.jsx
+++ client/src/components/Dashboard-Contents.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-import { UserCards } from "./UserCards";
+// import { UserCards } from "./UserCards";
 import NewCard from "./NewCard";
 import { Leaderboard } from "./Leaderboard";
-import { CardCollection } from "./CardCollection";
-import UserProfile from "./UserProfile";
+import CardCollection from "./CardCollection";
+import UserProfile from "./UserProfile"; 
 
 // the container for all of the pages in the user dashboard
 // changes based on the currentContent state in App.js
 export const DashboardContents = props => (
   <div className="dashboard-contents">
     {props.currentContent === "user-cards" ? (
-      <UserCards {...props} />
+      <CardCollection classname={'user-cards'} collection={props.userCards} {...props} />
     ) : props.currentContent === "card-collection" ? (
-      <CardCollection {...props} />
+      <CardCollection classname={'card-collection'} collection={props.cards} {...props} />
     ) : props.currentContent === "get-new-card" ? (
       <NewCard {...props} />
     ) : props.currentContent === "user-profile" ? (
